94ca068d1ad7ae7713d6a68186981ab2cd8cb2c0
pinopinopi <kaxp1685@yahoo.co.jp>
2018-10-15 14:07:30 +0000

    Tasklist Alomost Done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,9 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
-   @tasks = Task.all.page(params[:page])
+   @tasks = current_user.tasks.page(params[:page])
   end
 
   def show
@@ -14,7 +15,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = 'タスクが投稿されました'
@@ -53,7 +54,7 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
   
- def task_params
-   params.require(:task).permit(:content, :status)
- end
+  def task_params
+    params.require(:task).permit(:content, :status)
+  end
 end
